b9468c5fee2d477a5f3827d06d2357e2fb15129e
gabrieln96 <gabrielaraujo.n96@gmail.com>
2023-02-02 19:51:27 -0300

    resgatando user autenticado

--- middlewares/authGuard.js
+++ middlewares/authGuard.js
@@ -1,27 +1,26 @@
-const User = require("../models/User")
-const jwt = require("jsonwebtoken")
-const jwtSecret = process.env.JWT_SECRET
+const User = require("../models/User");
+
+const jwt = require("jsonwebtoken");
+
+const jwtSecret = process.env.JWT_SECRET;
 
 const authGuard = async (req, res, next) => {
-    const authHeader = req.headers["authorizarion"]
-    const token = authHeader && authHeader.split(" ")[1]
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    //check header has a token
-    if(!token) return res.status(401).json({errors: ["Acesso negado!"]})
+  // Check if header has a token
+  if (!token) return res.status(401).json({ errors: ["Acesso negado!"] });
 
-    //check if token is valid
-    try {
-        
-        const verified = jwt.verify(token, jwtSecret)
+  // Check if token is valid
+  try {
+    const verified = jwt.verify(token, jwtSecret);
 
-        req.user = await User.findById(verified.id).select("-password")
+    req.user = await User.findById(verified.id).select("-password");
 
-        next();
+    next();
+  } catch (err) {
+    res.status(400).json({ errors: ["O Token é inválido!"] });
+  }
+};
 
-    } catch (error) {
-        res.status(401).json({errors: ["Token inválido!"]})
-        
-    }
-}
-
-module.exports = authGuard
+module.exports = authGuard;
--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,7 +2,8 @@ const express = require("express")
 const router = express.Router()
  
 // Controller
-const { register, login } = require("../controllers/UserController")
+const { register, login, getCurrentUser } = require("../controllers/UserController")
+const authGuard = require("../middlewares/authGuard")
  
 // Middlewares
 const validate = require("../middlewares/handleValidation")
@@ -11,5 +12,6 @@ const {userCreateValidation, loginValidation } = require("../middlewares/userVal
 // Routes
 router.post("/register", userCreateValidation(), validate, register)
 router.post("/login", loginValidation(), validate, login)
+router.get("/profile", authGuard, getCurrentUser )
  
 module.exports = router
