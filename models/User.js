74c41ebae11f2d81037f4863242f6b5895f007de
gabrieln96 <gabrielaraujo.n96@gmail.com>
2023-02-02 15:04:48 -0300

    criando controller do usuario

--- models/User.js
+++ models/User.js
@@ -1,18 +1,19 @@
-const mongoose = require("mongoose")
-const {Schema} = mongoose
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const userSchema = mew Schema({
+const userSchema = new Schema(
+  {
     name: String,
     email: String,
     password: String,
     profileImage: String,
-    bio: String
-}, 
-    {
-        timestamps: true
-    }
+    bio: String,
+  },
+  {
+    timestamps: true,
+  }
 );
 
-const User = mongoose.model("User", userSchema);
+User = mongoose.model("User", userSchema);
 
 module.exports = User;
--- routes/Router.js
+++ routes/Router.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const router = express();
-
-// test router
-
-
+const express = require("express")
+const router = express()
+ 
+router.use("/api/users", require("./UserRoutes"))
+ 
+// test route
 router.get("/", (req, res) => {
-    res.send("API WORKING!");
-});
-
-module.exports = router ;
+    res.send("API Working!")
+})
+ 
+module.exports = router
