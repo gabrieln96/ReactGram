b0439e94c62ce81196bc069fd194b1c2a20c094a
gabrieln96 <gabrielaraujo.n96@gmail.com>
2023-02-03 18:35:06 -0300

    excluindo fotos

--- controllers/PhotoController.js
+++ controllers/PhotoController.js
@@ -26,6 +26,7 @@ const insertPhoto = async(req, res) => {
         res.status(422).json({
             errors: ["Houve um problema, por favor tente novamente."]
         })
+        return
     }
 
     res.status(201).json(newPhoto)
@@ -33,6 +34,37 @@ const insertPhoto = async(req, res) => {
     res.send("Photo insert")
 }
 
+
+// Remove photo
+const deletePhoto = async(req, res) => {
+
+    const {id} = req.params 
+
+    const reqUser = req.user 
+
+    try {
+        const photo = await Photo.findById(mongoose.Types.ObjectId(id))
+
+        // Check if photo exists
+        if (!photo) {
+            res.status(404).json({ errors: ["Foto não encontrada!"]})
+            return
+        }
+    
+        // Check if photo to user
+        if (!photo.userId.equals(reqUser._id)) {
+            res.status(422).json({ errors: ["Ocorreu um erro, tente novamente mais tarde"]})
+        }
+    
+        await Photo.findByIdAndDelete(photo._id)
+            res.status(200).json({ id: photo._id, message: "Foto excluída com sucesso!"})
+    } catch (error) {
+        res.status(404).json({ errors: ["Foto não encontrada!"]})
+            return
+    }
+}
+
 module.exports = {
     insertPhoto,
+    deletePhoto,
 }
